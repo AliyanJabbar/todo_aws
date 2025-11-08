949e1f85ab0dc7a89628945c55d69d33dc41dfaa
Aliyan Jabbar <jabbaraliyan805@gmail.com>
2025-11-08 14:54:06 +0500

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:postgres@database-1.cluster-cpoosiywaiqv.eu-north-1.rds.amazonaws.com:5432/database-1-instance-1")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://todoappuser:todoapppw@todoappdb.c3so26oscjij.ap-southeast-2.rds.amazonaws.com:5432/todoappdb")
 
 # Create engine and session factory
 engine = create_engine(DATABASE_URL, future=True)
